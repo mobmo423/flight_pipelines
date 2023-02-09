4fd51cd6b95878efbd78bce73f2680ec073b91a3
Marc Medawar <marcmedawar14@gmail.com>
2023-02-09 04:15:28 -0800

    added data quality test

--- project/src/pipeline/transform_pipeline.py
+++ project/src/pipeline/transform_pipeline.py
@@ -2,6 +2,7 @@ from database.postgres import PostgresDB
 from pipeline.extract_load_pipeline import ExtractLoad
 from flights.etl.transform import Transform
 import logging
+from data_quality_test.test_load import TestLoad 
 from graphlib import TopologicalSorter
 
 
@@ -21,7 +22,9 @@ def run_pipeline():
     logging.info("Creating extract and load nodes")
     node_extract_load = ExtractLoad(source_engine=source_engine,target_engine=target_engine,table_name='flight_data',path=path_extract_model)
     dag.add(node_extract_load)
-
+    
+    data_quality_test = TestLoad(date = '2023-02-06',table_name='flight_data',engine = source_engine,num_record=4)
+    dag.add(data_quality_test,node_extract_load)
     logging.info("Creating Transform and load nodes")
     node_staging_flight = Transform(table_name='staging_flights',engine=target_engine,models_path=path_transform_model)
     #node_serving_films_popular = Transform(...)
@@ -36,6 +39,7 @@ def run_pipeline():
         node.run()
 
     logging.info("Pipeline run successful")
+
 
  
 if __name__ == "__main__":
